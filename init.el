9f010db912b92ec3a5f8f059eda59fde035f52e9
kaiyu.shan <kaiyu.shan@outlook.com>
2025-01-25 13:40:48 +0800

    feat(spacemacs): 删除无用package

--- init.el
+++ init.el
@@ -32,8 +32,7 @@ This function should only modify configuration layer settings."
 
    ;; List of configuration layers to load.
    dotspacemacs-configuration-layers
-   '(html
-     (markdown :variables markdown-live-preview-engine 'vmd)
+   '(markdown
      ;; ----------------------------------------------------------------
      ;; Example of useful layers you may want to use right away.
      ;; Uncomment some layer names and press `SPC f e R' (Vim style) or
